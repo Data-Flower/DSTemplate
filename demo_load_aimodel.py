b3fa0f561663d5cd7eba7844bc6c68dd4fd8ada7
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-06-07 12:28:43 +0900

    0.0.3 feat: create demo code to underestand load aimodel in AWS S3

---
+++ demo_load_aimodel.py
@@ -0,0 +1,11 @@
+from DS import Loads
+
+def demo_load_aiModel():
+    import Datas
+    aws_access_key_id, aws_secret_access_key, aws_s3_bucket_name = Datas.data_from_env()
+
+    aimodel = '대충 AI모델 데이터 내용'
+    filename = 'AI_model.txt'
+
+    Loads.load_AIModel(aimodel, filename, 
+                       aws_access_key_id, aws_secret_access_key, aws_s3_bucket_name)
